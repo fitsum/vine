89a2c9c948f179d425e64415bfa3c6aafa29286e
fitsum <fitsumbelay@gmail.com>
2013-12-26 18:22:37 -0500

    formatting and new README

---
+++ README.md
@@ -0,0 +1,18 @@
+VINE API
+========
+
+[DEMO](http://dumbservices.com/t/vine/)
+
+##SHORT TODO
+- UI
+	- transition
+	- controls
+	- (info?)
+- add geo & orientation
+
+
+##LONG TODO
+
+- UI
+	- transitions
+	- comments
--- index.php
+++ index.php
@@ -16,10 +16,10 @@
         $ch = curl_init(); 
 
         // set url and post parameters 
-		$data = array('username' => 'fitsumbelay@gmail.com', 'password' => '8JeuKHvR');
+	$data = array('username' => 'fitsumbelay@gmail.com', 'password' => '8JeuKHvR');
         curl_setopt($ch, CURLOPT_URL, "https://api.vineapp.com/users/authenticate"); 
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
         //return the transfer as a string 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
@@ -42,15 +42,15 @@
 
         // set url and headers 
         curl_setopt($ch, CURLOPT_URL, "https://api.vineapp.com/timelines/popular"); 
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');        
-		curl_setopt($ch, CURLOPT_HTTPHEADER,array($curl_headers));
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');        
+	curl_setopt($ch, CURLOPT_HTTPHEADER,array($curl_headers));
 
         //return the transfer as a string 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 
         // $output contains the output string 
         $vine_request = curl_exec($ch); 
-		$vine_request = str_replace('\n', '', $vine_request);
+	$vine_request = str_replace('\n', '', $vine_request);
         echo $vine_request;
 
         // close curl resource to free up system resources 
